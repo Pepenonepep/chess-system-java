4397ccafda2840aee395116f51279a5d212c12be
Cauã Oliveira <caua.olivergomes22@outlook.com>
2026-08-23 20:57:09 -0300

    BoardException and defensive programming

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -24,8 +24,8 @@ public ChessPiece[][] getPieces(){
 }
 
 private void initialSetup(){
-    board.placePiece(new Rook(board, Color.WHITE), new Position(2,4));
-    board.placePiece(new King(board, Color.WHITE), new Position(2,5))   ;
+    board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
+    board.placePiece(new King(board, Color.BLACK), new Position(3,5))   ;
 }
 
 }
