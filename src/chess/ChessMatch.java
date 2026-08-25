da63293031c57df2fa179d38542c60afebdea891
Seu Nome <cauacontato06@outlook.com>
2026-08-25 18:10:56 -0300

    ChessException and ChessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -6,26 +6,30 @@ import chess.pieces.King;
 import chess.pieces.Rook;
 
 public class ChessMatch {
-private Board board;
+    private Board board;
 
-public ChessMatch(){
-    board = new Board(8,8);
-    initialSetup();
-}
-
-public ChessPiece[][] getPieces(){
-    ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
-    for(int i=0;i<board.getRows();i++){
-        for(int j=0;j<board.getColumns();j++){
-            mat[i][j] = (ChessPiece) board.piece(i,j);
-        }
+    public ChessMatch() {
+        board = new Board(8, 8);
+        initialSetup();
     }
-    return mat;
-}
 
-private void initialSetup(){
-    board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
-    board.placePiece(new King(board, Color.BLACK), new Position(3,5))   ;
-}
+    public ChessPiece[][] getPieces() {
+        ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
+        for (int i = 0; i < board.getRows(); i++) {
+            for (int j = 0; j < board.getColumns(); j++) {
+                mat[i][j] = (ChessPiece) board.piece(i, j);
+            }
+        }
+        return mat;
+    }
+
+    private void placeNewPiece(char column, int row, ChessPiece piece) {
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+
+    private void initialSetup() {
+        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+        placeNewPiece('h', 2, new King(board, Color.BLACK));
+    }
 
 }
