abe1e4cbfb87dde4990bea63105e176975eec262
Seu Nome <cauacontato06@outlook.com>
2026-08-26 22:02:52 -0300

    Move Pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,23 @@
 package application;
 
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
-     ChessMatch chessMatch = new ChessMatch();
-     UI.printBoard(chessMatch.getPieces());
+        ChessMatch chessMatch = new ChessMatch();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Souce: ");
+            ChessPosition source = UI.readChessPosition(sc);
+            System.out.print("Target: ");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
     }
 }
-
